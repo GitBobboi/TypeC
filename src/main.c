74290c43eb27ab5eff1d659994a6d78a2c905f50
joneslogan1106 <loganjones110613@gmail.com>
2025-05-06 02:27:20 +0000

    Added tokenization and reading of files, still some bugs happened

--- src/main.c
+++ src/main.c
@@ -18,6 +18,46 @@ void add_data(void* array, char* data, size_t size) {
     memcpy(array, data, size);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[]) {
+    char filename[1024];
+    strcpy(filename, argv[1]);
+    printf("Arg: %s\n", argv[1]);
+    char content[4096];
+
+    FILE *pF = fopen(argv[1], "r");
     
+    char buffer[255];
+
+    if(pF == NULL) 
+    {
+        printf("Unable to open file!\n");
+    }
+    else
+    {
+        while(fgets(buffer, 255, pF) != NULL)
+        {
+            printf("%s\n", buffer);
+            if (strcmp(buffer, "\n") == 0) {
+                continue;
+            } else {
+                strcpy(content, buffer);
+            }
+        }
+    }
+
+    char delimiter[] = " ";
+    char *token;
+
+    token = strtok(content, delimiter);
+
+    while (token != NULL) {
+        printf("Token: %s\n", token);
+        token = strtok(NULL, delimiter);
+    }
+
+
+
+    fclose(pF);
+
+    return 0;
 }
